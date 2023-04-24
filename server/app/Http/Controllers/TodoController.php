663105d347e578e1e9c82c1a14b87116ea2a6dcb
emroz <fahimalemroz@gmail.com>
2023-04-24 22:23:53 +0600

    Formatted and improved

--- server/app/Http/Controllers/TodoController.php
+++ server/app/Http/Controllers/TodoController.php
@@ -7,34 +7,40 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $todos = Todo::all();
         return response()->json($todos);
     }
 
-    public function show(Todo $todo) {
+    public function show(Todo $todo)
+    {
         return $todo;
     }
 
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
         $new_todo = Todo::create([
-            'name' => $request->input('name'),
-            'is_completed' => $request->input('is_completed'),
+            'name' => $request->name,
+            'is_completed' => $request->is_completed,
         ]);
 
         return $new_todo;
     }
 
-    public function update(Request $request, Todo $todo) {
-        $todo->name = $request->input('name');
-        $todo->is_completed = $request->input('is_completed');
-        $todo->save();
+    public function update(Request $request, Todo $todo)
+    {
+        $todo->update([
+            'name' => $request->name,
+            'is_completed' => $request->is_completed,
+        ]);
 
         return $todo;
     }
 
-    public function destroy(Todo $todo) {
+    public function destroy(Todo $todo)
+    {
         $todo->delete();
-        return $todo;
+        return response()->json(['msg' => 'Todo deleted']); 
     }
 }
