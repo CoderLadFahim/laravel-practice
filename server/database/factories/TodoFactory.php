f2ba5f6f675360b2969c7f8024771e8191e721c0
emroz <fahimalemroz@gmail.com>
2023-04-24 19:17:03 +0600

    Populated the db with a factory and a seeder

---
+++ server/database/factories/TodoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AppModelsTodo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Todo::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'is_completed' => $this->faker->boolean(),
+        ];
+    }
+}
